506b78e677c36deea1e0327fc382df6ee9e5fbe5
S. Andrew Sheppard <andrew@wq.io>
2013-07-01 15:00:07 -0500

    use pragmas; don't touch start.js/end.js

--- smash2amd.py
+++ smash2amd.py
@@ -4,6 +4,8 @@ import re
 
 def smash2amd(filename):
     name = os.path.splitext(os.path.basename(filename))[0]
+    if name in ('start', 'end'):
+        return
     definere = re.compile(r'^define\(')
     importre = re.compile(r'^import "(.+)";')
     exportre1 = re.compile(r'^(d3.%s) = ' % name)
@@ -13,6 +15,7 @@ def smash2amd(filename):
     rows = []
     deps = []
     export = ""
+    base = ""
     for row in code.split('\n'):
         if definere.match(row):
             print "Already AMD!"
@@ -20,6 +23,12 @@ def smash2amd(filename):
         match = importre.match(row)
         if match:
             dep = match.group(1)
+            if dep == "end":
+                continue
+            if dep == "start":
+                dep = "base"
+                export = "d3"
+                base = "d3"
             if not dep.startswith("../"):
                 dep = "./%s" % dep
             if dep.endswith('/'):
@@ -29,6 +38,8 @@ def smash2amd(filename):
             match = exportre1.match(row)
             if match:
                 export = match.group(1)
+                deps.insert(0, "base")
+                base = "d3"
             else:
                 match = exportre2.match(row)
                 if match and not export.startswith('d3.'):
@@ -36,11 +47,7 @@ def smash2amd(filename):
                 
             rows.append(row)
     
-    base = ''
     if export:
-        if export.startswith('d3.'):
-            deps.insert(0, 'base')
-            base = 'd3'
         export = "\nreturn %s;" % export
 
     if len(deps) > 0:
@@ -50,13 +57,13 @@ def smash2amd(filename):
 
     out = open(filename, 'w')
     out.write(
-"""/* start amd */
+"""//>>excludeStart("amd", pragmas.amd)
 define(%sfunction(%s) {
-/* end amd */
+//>>excludeEnd("amd")
 %s
-/* start amd */%s
+//>>excludeStart("amd", pragmas.amd)%s
 });
-/* end amd */""" % (
+//>>excludeEnd("amd")""" % (
         depstr,
         base,
         "\n".join(rows),
